18b09262a77437a3565811e2064dfec1658396fe
Ghaffaru <mudashiruagm@gmail.com>
2020-08-28 16:08:57 +0000

    added superclass Vehicle for inheritance

---
+++ inheritance/Vehicle.php
@@ -0,0 +1,65 @@
+<?php
+
+// Super Class
+class Vehicle
+{
+    private $noOfWheels;
+    private $color;
+    private $fuel;
+    private $speed;
+
+    public function getNoofWheels()
+    {
+
+    }
+
+    public function setNoOfWheels($wheels)
+    {
+
+    }
+
+    public function getColor()
+    {
+
+    }
+
+    public function setColor($color)
+    {
+
+    }
+
+    public function getFuel()
+    {
+
+    }
+
+    public function setFuel($fuel)
+    {
+
+    }
+
+    public function getSpeed()
+    {
+
+    }
+
+    public function setSpeed($speed)
+    {
+
+    }
+
+    public function start()
+    {
+        echo "Vechicle is starting...<br/>";
+    }
+
+    public function accelerate()
+    {
+        echo "Vehicle is accelerating...<br/>";
+    }
+
+    public function brake()
+    {
+        echo "Vehicle is stoping...<br/>";
+    }
+}
